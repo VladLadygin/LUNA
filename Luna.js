fa7b457876104f11d3ef0a71077c4ed8498ee7fb
VladLadygin <136591866+VladLadygin@users.noreply.github.com>
2023-08-08 11:20:30 +0300

    Add files via upload

---
+++ Luna.js
@@ -0,0 +1,19 @@
+const HelloVueApp = {
+    data() {
+        return {
+            link_on_registration: 'https://oauth.vk.com/authorize?client_id=51696427&redirect_uri=https://vladladygin.github.io/LUNA/Luna.html&display=popup&scope=friends&response_type=token&v=5.131',
+            link_with_token: 'mm',
+            m: ''
+
+        }
+    },
+    methods: {
+        registracia_vk() {
+            window.location.href = this.link_on_registration
+            this.link_with_token = window.location.href
+            return this.link_with_token
+        }
+    }
+}
+
+Vue.createApp(HelloVueApp).mount('#all')
