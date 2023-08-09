00bacb839d7bc531b96e9a6d7552169dcefca456
VladLadygin <byrims@mail.ru>
2023-08-09 11:24:42 +0300

    add link_with_token

--- Luna.js
+++ Luna.js
@@ -2,16 +2,19 @@ const HelloVueApp = {
     data() {
         return {
             link_on_registration: 'https://oauth.vk.com/authorize?client_id=51696427&redirect_uri=https://vladladygin.github.io/Luna.html&display=popup&scope=friends&response_type=token&v=5.131',
-            link_with_token: 'mm',
+            link_with_token: '',
             m: ''
 
         }
     },
+    created() {
+        this.link_with_token = window.location.href;
+        console.log(window.location.href)
+        console.log(this.link_with_token)
+    },
     methods: {
         registracia_vk() {
-            window.location.href = this.link_on_registration
-            this.link_with_token = window.location.href
-            return this.link_with_token
+            window.location.href = this.link_on_registration;
         }
     }
 }
