558a941543c8c877095d67ae810908e446f02275
VladLadygin <byrims@mail.ru>
2023-08-15 11:45:19 +0300

    otrisovka spiska

--- Luna.js
+++ Luna.js
@@ -1,14 +1,14 @@
 const HelloVueApp = {
     data() {
         return {
-            jsonobjects: null
+            jsonobject: null
         }
     },
     methods: {
         auth() {
             VK.Api.call('friends.get', { fields: 'photo_100', order: 'hints', v: '5.131' }, function (response) {
                 this.jsonobject = response;
-                console.log(this.json_object);
+                console.log(this.jsonobject);
             });
 
         }
