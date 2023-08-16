fde0ec102895b4500eb7143e4d704f8a9106d46d
VladLadygin <byrims@mail.ru>
2023-08-16 15:58:46 +0300

    otrisovka spiska s usloviem(8) JSON.parse

--- Luna.js
+++ Luna.js
@@ -8,12 +8,13 @@ const HelloVueApp = {
     methods: {
         auth() {
             VK.Api.call('friends.get', { fields: 'photo_100', order: 'hints', v: '5.131' }, function (response) {
-                this.jsonobject = JSON.parse(response);
+                this.jsonobject = response;
                 console.log(this.jsonobject);
                 console.log(response);
+                console.log(this.show_friends);
                 this.show_friends = true;
+                console.log(this.show_friends);
             })
-            console.log(this.show_friends);
         }
     }
 }
