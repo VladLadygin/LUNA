3595a056216b78d6410cd53c17d4aaa58c76d1f9
VladLadygin <byrims@mail.ru>
2023-08-16 15:32:32 +0300

    otrisovka spiska s usloviem(5)

--- Luna.js
+++ Luna.js
@@ -10,8 +10,11 @@ const HelloVueApp = {
             VK.Api.call('friends.get', { fields: 'photo_100', order: 'hints', v: '5.131' }, function (response) {
                 this.jsonobject = response;
                 console.log(this.jsonobject);
+                console.log(response);
+                console.log(this.show_friends);
             });
             this.show_friends = true;
+            console.log(this.show_friends);
         }
     }
 }
