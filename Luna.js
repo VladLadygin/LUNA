2f43c3db4ed11a3ea7036b08b350b08dcb6e1945
VladLadygin <byrims@mail.ru>
2023-08-17 13:32:54 +0300

    otrisovka spiska i propaza knopki

--- Luna.js
+++ Luna.js
@@ -2,24 +2,22 @@ const HelloVueApp = {
     data() {
         return {
             jsonobject: null,
-            show_friends: false
+            show_friends: false,
+            vk_button_seen: true
         }
     },
     methods: {
         auth() {
             VK.Api.call('friends.get', { fields: 'photo_100', order: 'hints', v: '5.131' }, (response) =>  {
                 this.jsonobject = response;
-                console.log(this.show_friends);
-                this.show_friends = true;
                 console.log(this.jsonobject);
                 console.log(this.show_friends);
+                this.show_friends = true;
+                console.log(this.show_friends);
+                console.log(this.vk_button_seen);
+                this.vk_button_seen = false;
+                console.log(this.vk_button_seen);           
             })
-        },
-        ppp() {
-            this.show_friends = true;
-            console.log(this.jsonobject);
-            console.log(this.show_friends);
-
         }
     },
     mounted() {
